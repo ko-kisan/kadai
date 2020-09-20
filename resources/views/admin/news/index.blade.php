3949712209d15a63a7ebe176fcd72bdb07dab7e5
ko-kisan <03t08k27@gmail.com>
2020-09-20 21:50:27 +0900

    1.4

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -13,9 +13,45 @@
 			<div class="col-md-8">
 				<form class="" action="{{action('Admin\NewsController@index')}}" method="get">
 					<div class="form-group row">
-						<label class="col-md-2" for=""></label>
+						<label class="col-md-2" for="">タイトル</label>
+						<div class="col-md-8">
+							<input type="text" class="form-control" name="cond_title" value="{{$cond_title}}">
+
+						</div>
+						<div class="col-md-2">
+							{{cerf_field()}}
+							<input type="submit" name="btn btn-primary" value="検索">
+						</div>
 					</div>
 				</form>
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="list-news col-md-12 mx-auto">
+				<div class="row">
+					<table class="table table-dark">
+						<thead>
+							<tr>
+								<th width="10%">ID</th>
+								<th width="20%">タイトル</th>
+								<th width="50%">本文</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($posts as $news)
+							<tr>
+								<th>{{$news->id}}</th>
+								<td>{{str_limit($news->title,100)}}</td>
+								<td>{{str_limit($news->body,250)}}</td>
+							</tr>
+
+							@endforeach
+						</tbody>
+					</table>
+
+				</div>
+			</div>
+		</div>
 	</div>
+	@endsection
